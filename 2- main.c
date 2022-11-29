86054172f589093bc31d44f8231f9b5cd9caea3b
soooyaya <hsoo0531@naver.com>
2022-11-29 18:50:03 +0900

    01

---
+++ 2- main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct point{
+	int x;
+	int y;
+};
+
+int main(int argc, char *argv[]) {
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double distance;
+	
+	printf("input p1 coordinate (x y) : ");
+	scanf("%d %d", &p1.x, &p1.y);
+	
+	printf("input p2 coordinate (x y) : ");
+	scanf("%d %d", &p2.x, &p2.y);
+
+	distance = sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+	printf("distance : %f\n", distance);
+	
+
+	return 0;
+}
+
+
+
